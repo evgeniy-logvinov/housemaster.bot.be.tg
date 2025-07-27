c7ddc2e76bd4f4755effd41fa066ce43a77478d4
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2025-07-28 03:18:15 +0500

    v1.0.21 - update keyboard titles

--- src/handlers/keyboard.ts
+++ src/handlers/keyboard.ts
@@ -7,15 +7,15 @@ const t = translations[language]; // Select translations based on the language
 export const mainKeyboard = {
   reply_markup: {
     keyboard: [
-      [{ text: t.addMeAsResident }], // Button to add the current user as a resident
-      [{ text: t.removeMeAsResident }], // Button to remove the current user as a resident
-      [{ text: t.getResidentsByApartment }], // Button to get residents by apartment number
-      [{ text: t.addResident }], // Button to add another user as a resident
-      [{ text: t.removeResidentByName }], // Button to remove another user by name
-      [{ text: t.addPhoneNumber }], // Button to add a phone number
-      [{ text: t.removePhoneNumber }], // Button to remove a phone number
       [{ text: t.generateBuildingImage }], // Button to generate and show building image
       [{ text: t.generateFloorImage }], // Новая кнопка
+      [{ text: t.getResidentsByApartment }], // Button to get residents by apartment number
+      [{ text: t.addMeAsResident }], // Button to add the current user as a resident
+      [{ text: t.removeMeAsResident }], // Button to remove the current user as a resident
+      [{ text: t.addPhoneNumber }], // Button to add a phone number
+      [{ text: t.removePhoneNumber }], // Button to remove a phone number
+      [{ text: t.addResident }], // Button to add another user as a resident
+      [{ text: t.removeResidentByName }], // Button to remove another user by name
       [{ text: t.closeKeyboard }], // Новая кнопка
     ],
     resize_keyboard: true,
